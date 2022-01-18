4daaeedcf6b14cc5ddd0ed3f7cd7419d97aa00a1
tommasobenedetti <tommasobenedetti89@gmail.com>
2022-01-18 10:51:15 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MainView from './components/main-view/main-view';
-import Container from 'react-bootstrap/Container';
-
-// Import statement to indicate that you need to bundle `./index.scss`
-import './index.scss';
+import Container from 'react-bootstrap';
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
