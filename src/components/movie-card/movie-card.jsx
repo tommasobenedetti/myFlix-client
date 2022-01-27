8628092d943fbff143d39bf4c6b2502870d52a5e
tommasobenedetti <tommasobenedetti89@gmail.com>
2022-01-27 17:38:09 +0100

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -5,7 +5,7 @@ import Card from 'react-bootstrap/Card';
 
 import { Link } from "react-router-dom";
 
-export class MovieCard extends React.Component {
+export default class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
 
