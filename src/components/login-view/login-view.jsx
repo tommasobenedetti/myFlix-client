2675bd1e28f236b691979ecea2a58588455833aa
tommasobenedetti <tommasobenedetti89@gmail.com>
2022-05-24 18:15:37 +0200

    Fixing minor flaws in login/registrtation page, still not wroking

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -28,8 +28,8 @@ export default function LoginView(props) {
     if (!password) {
       setPasswordErr('Password Required');
       isReq = false;
-    } else if (password.length < 6) {
-      setPassword('Password must be 6 characters long');
+    } else if (password.length < 5) {
+      setPassword('Password must be 5 characters long');
       isReq = false;
     }
 
--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -5,35 +5,35 @@ import { Link, Router } from 'react-router-dom';
 import { Form, Button, Container, Row, Col, CardGroup, Card } from 'react-bootstrap';
 
 export function RegistrationView(props) {
-    const [Username, setUsername] = useState('');
-    const [Password, setPassword] = useState('');
-    const [Email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
 
-    const [UsernameErr, setUsernameErr] = useState('');
-    const [PasswordErr, setPasswordErr] = useState('');
-    const [emailErr, setemailErr] = useState('');
+    const [usernameErr, setUsernameErr] = useState('');
+    const [passwordErr, setPasswordErr] = useState('');
+    const [emailErr, setEmailErr] = useState('');
 
     const validate = () => {
 
         let isReq = true;
-        if (!Username) {
+        if (!username) {
             setUsernameErr('Username Required');
             isReq = false;
         } else if (Username.length < 6) {
             setUsernameErr('Username must be 6 characters long');
             isReq = false;
         }
-        if (!Password) {
+        if (!password) {
             setPasswordErr('Password Required');
             isReq = false;
         } else if (Password.length < 8) {
             setPasswordErr('Password must be 8 characters long');
             isReq = false;
         }
-        if (!Email) {
-            setemailErr('Please enter a valid email')
+        if (!email) {
+            setEmailErr('Please enter a valid email')
         } else if (Email.indexOf('@') === -1) {
-            setemailErr('Please enter a valid email')
+            setEmailErr('Please enter a valid email')
             isReq = false;
         }
         return isReq;
@@ -43,7 +43,7 @@ export function RegistrationView(props) {
     const handleRegister = (e) => {
         e.preventDefault();
         const isReq = validate();
-        console.log(Username, Password)
+        console.log(username, password)
         if (isReq) {
             axios.post('https://quiet-savannah-08380.herokuapp.com/users', {
                 Username: Username,
@@ -79,28 +79,28 @@ export function RegistrationView(props) {
                                             <Form.Label> Username: </Form.Label>
                                             <Form.Control
                                                 type="text"
-                                                value={Username}
+                                                value={username}
                                                 onChange={e => setUsername(e.target.value)}
                                                 required
                                                 placeholder="Enter a username" />
-                                            {UsernameErr && <p>{UsernameErr}</p>}
+                                            {usernameErr && <p>{usernameErr}</p>}
                                         </Form.Group>
                                         <Form.Group>
                                             <Form.Label>Password:</Form.Label>
                                             <Form.Control
                                                 type="password"
-                                                value={Password}
+                                                value={password}
                                                 onChange={e => setPassword(e.target.value)}
                                                 required
                                                 minLength="5"
                                                 placeholder="Your password must be at least 5 characters" />
-                                            {PasswordErr && <p>{PasswordErr}</p>}
+                                            {passwordErr && <p>{passwordErr}</p>}
                                         </Form.Group>
                                         <Form.Group>
                                             <Form.Label>Email:</Form.Label>
                                             <Form.Control
                                                 type="text"
-                                                value={Email}
+                                                value={email}
                                                 onChange={e => setEmail(e.target.value)}
                                                 required
                                                 placeholder="Enter your email" />
