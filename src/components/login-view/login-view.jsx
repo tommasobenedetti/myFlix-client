071b3d9dc8c1cd4c0ea8593536e372e85a1cf472
tommasobenedetti <tommasobenedetti89@gmail.com>
2022-06-15 09:53:00 +0200

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -2,10 +2,32 @@ import axios from "axios";
 import React, { useState } from "react";
 import { Form, Button, Container, Row, Col, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 export default function LoginView(props) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  // validate user inputs
+  const validate = () => {
+    let isReq = true;
+    if (!username) {
+      setUsernameErr('Username Required');
+      isReq = false;
+    } else if (username.length < 2) {
+      setUsernameErr('Username must be 2 characters long');
+      isReq = false;
+    }
+    if (!password) {
+      setPasswordErr('Password Required');
+      isReq = false;
+    } else if (password.length < 6) {
+      setPassword('Password must be 6 characters long');
+      isReq = false;
+    }
+
+    return isReq;
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -26,36 +48,38 @@ export default function LoginView(props) {
       });
   };
   return (
-    <Container>
-      <Row>
-        <Col>
-          <Card style={{ marginTop: 100, marginBottom: 50, width: '30' }}>
-            <Card.Body>
-              <Card.Title style={{ textAlign: "center", fontSize: "17px" }}>
-                <Form className='login-border'>
-                  <Form.Group controlId="formUsername">
-                    <Form.Label>Username:</Form.Label>
-                    <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} />
-                  </Form.Group>
-                  <Form.Group controlId="formPassword">
-                    <Form.Label>Password:</Form.Label>
-                    <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
-                  </Form.Group>
-                  <Button variant="primary" type="submit" onClick={handleSubmit}>
-                    Submit
-                  </Button>
-                  <Link to={'/register'}>
-                    <Button className="loginButtons" variant="primary">
-                      Register
-                    </Button>
-                  </Link>
-                  <br></br> <br></br>
-                </Form>
-              </Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+    <Form>
+      <Form.Group controlId="formUsername">
+        <br></br><br></br>
+        <Form.Label className="loginForm">Username: </Form.Label>
+        <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+      </Form.Group>
+      <br></br>
+      <Form.Group controlId="formPassword">
+        <Form.Label className="loginForm">Password: </Form.Label>
+        <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+      </Form.Group>
+      <br></br>
+      <Button variant="primary" type="submit" onClick={handleSubmit}>
+        Login
+      </Button>
+      <br></br>
+
+      <Link to={'/register'}>
+        <Button className="loginButtons" variant="primary">
+          Register
+        </Button>
+      </Link>
+      <br></br> <br></br>
+    </Form>
   );
 }
+
+
+LoginView.propTypes = {
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired,
+  }),
+  onLoggedIn: PropTypes.func.isRequired,
+};
